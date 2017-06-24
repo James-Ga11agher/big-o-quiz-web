89cd303c86a9cd9171f46c23010fa125879e4999
James Gallagher <james.gallagher@gmail.com>
2017-06-24 21:05:45 +0100

    Resolving issue loading bundle.js from Express server

--- servers/srcExpressServer.js
+++ servers/srcExpressServer.js
@@ -6,8 +6,12 @@ import path from 'path';
 let app = express();
 let port = 3000;
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join( __dirname, '../dist/index.html'));
+});
+
+app.get('/bundle.js', function(req, res) {
+  res.sendFile(path.join( __dirname, '../dist/bundle.js'));
 });
 
 app.listen(port, function(err) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
-//import App from './components/App';
+import App from './components/App';
 
 /* eslint-disable no-console */
 console.log('Render App 3');
